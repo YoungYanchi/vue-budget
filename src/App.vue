5c75d659150961e33624d498378c462198664a9a
Nikita Yanchevskii <lishslukh@gmail.com>
2023-01-22 16:31:41 +0300

    Form rdy

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,55 @@
 <template>
   <div id="app">
-    <BudgetList :list="list"/>
+    <FormCard @submitForm="onFormSubmit"/>
+    <TotalBalance :total="totalBalance"/>
+    <BudgetList :list="list" @deleteItem="onDeleteItem"/>
   </div>
 </template>
 
 <script>
 import BudgetList from "@/components/BudgetList";
-
+import TotalBalance from "@/components/TotalBalance";
+import FormCard from "@/components/FormCard";
 export default {
   name: 'App',
   components: {
     BudgetList,
+    TotalBalance,
+    FormCard
   },
   data: () => ({
     list: {
       1: {
-        type: 'INCOME',
-        value: 100,
-        comment: "Some comment",
+        type: 'Доходы',
+        value: 500,
+        comment: "Зарплата за январь",
         id: 1,
       },
       2: {
-        type: 'OUTCOME',
-        value: -50,
-        comment: "Some outcome comment",
+        type: 'Расходы',
+        value: -200,
+        comment: "Оплатил жкх",
         id: 2,
       },
     }
-  })
+  }),
+  computed: {
+    totalBalance() {
+      return Object.values(this.list).reduce((acc, item) => acc + item.value, 0)
+    }
+  },
+  methods: {
+    onDeleteItem(id) {
+      this.$delete(this.list, id )
+    },
+    onFormSubmit(data) {
+      const newObj = {
+        ...data,
+        id: String(Math.random())
+      }
+      this.$set(this.list, newObj.id, newObj)
+    }
+  }
 }
 </script>
 
---
+++ src/components/TotalBalance.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="total-value">
+    Balance: {{ total + '$' }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TotalBalance",
+  props: {
+    total: Number,
+    default: () => (0)
+  }
+}
+</script>
+
+<style scoped>
+  .total-value {
+    font-size: 20px;
+    text-transform: uppercase;
+    padding: 20px;
+    text-align: center;
+  }
+
+</style>
